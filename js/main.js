7858f6714e089cc71c22ece1476537994357bda7
Jesse Kevon <jessekevon@gmail.com>
2016-10-04 14:52:14 -0500

    added hamburger icon animation, cleaned up nav, removed some files

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ $(document).ready(function(){
 //   $( "#tabs" ).tabs();
 // } );
 
+// Init sliders
+
 $('.slider-one').slick({
 	arrows: false,
 	dots: false
@@ -18,37 +20,79 @@ $('.slider-two').slick({
 	dots: false
 });
 
+// Init Mobile Controls
 
-$('nav a').on('click', function() {
+function initMobileMenu() {
+ $('#triggerMobileMenu').on('click', function(){
+   $('.nav-wrap').toggleClass('active');
+   $('main').toggleClass('blur');
+   });
+}
+initMobileMenu();
 
-	$('nav').removeClass('activated');
+// function closeMobileMenu() {
+//  $('#closeMobileMenu').on('click', function(){
+//    $('.nav-wrap').removeClass('active');
+//    $('main').removeClass('blur');
+//    });
+// }
+// closeMobileMenu();
+
+function hamburgerAnimate() {
+ $('.hamburger').on('click', function(){
+   $(this).toggleClass('is-active');
+   });
+}
+hamburgerAnimate();
+
+
+// Smooth scrolling navigation
+
+$('.nav-items a').on('click', function() {
+	$('.nav-items').removeClass('active');
+
+	$('.nav-wrap').removeClass('active');
+	$('.hamburger').removeClass('is-active');
+	$('main').removeClass('blur');
 
     var scrollAnchor = $(this).attr('data-scroll'),
         scrollPoint = $('[data-anchor="' + scrollAnchor + '"]').offset().top - 50;
 
-    $('body,html').animate({
-        scrollTop: scrollPoint
-    }, 500);
+        if ($(window).width() > 1199) {
+
+        	$('body,html').animate({
+        	    scrollTop: scrollPoint
+        	}, 500);
+
+        } else {
+
+        	$('body,html').delay(500).animate({
+        	    scrollTop: scrollPoint
+        	}, 600);
+        }
 
     return false;
 
 })
 
+// Add / remove active class based on window position
 
 $(window).scroll(function() {
     var windscroll = $(window).scrollTop();
     if (windscroll >= 550) {
         $('.smooth-anchor').each(function(i) {
             if ($(this).position().top <= windscroll + 50) {
-                $('nav a.active').removeClass('active');
-                $('nav a').eq(i).addClass('active');
+                $('.nav-items a.active').removeClass('active');
+                $('.nav-items a').eq(i).addClass('active');
             }
         });
 
     } else {
 
-        $('nav').removeClass('fixed');
-        $('nav a.active').removeClass('active');
-        // $('nav a:first').addClass('active');
+        $('.nav-items').removeClass('fixed');
+        $('.nav-items a.active').removeClass('active');
+        $('.nav-items a:first').addClass('active');
     }
 });
+
+

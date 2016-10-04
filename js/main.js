4c061b7a6606a4c3ad43a755b68c70f857d37579
Jesse Kevon <jessekevon@gmail.com>
2016-10-04 10:24:46 -0500

    added smooth anchor nav functionality

--- js/main.js
+++ js/main.js
@@ -1,3 +1,54 @@
 $(document).ready(function(){
 	console.log('working');
 });
+
+// Init tabs
+
+// $( function() {
+//   $( "#tabs" ).tabs();
+// } );
+
+$('.slider-one').slick({
+	arrows: false,
+	dots: false
+});
+
+$('.slider-two').slick({
+	arrows: false,
+	dots: false
+});
+
+
+$('nav a').on('click', function() {
+
+	$('nav').removeClass('activated');
+
+    var scrollAnchor = $(this).attr('data-scroll'),
+        scrollPoint = $('[data-anchor="' + scrollAnchor + '"]').offset().top - 50;
+
+    $('body,html').animate({
+        scrollTop: scrollPoint
+    }, 500);
+
+    return false;
+
+})
+
+
+$(window).scroll(function() {
+    var windscroll = $(window).scrollTop();
+    if (windscroll >= 550) {
+        $('.smooth-anchor').each(function(i) {
+            if ($(this).position().top <= windscroll + 50) {
+                $('nav a.active').removeClass('active');
+                $('nav a').eq(i).addClass('active');
+            }
+        });
+
+    } else {
+
+        $('nav').removeClass('fixed');
+        $('nav a.active').removeClass('active');
+        // $('nav a:first').addClass('active');
+    }
+});
